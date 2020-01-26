b0eaa2d6568a677825bc025f91f6ef1d0a04b036
Katarzyna Jaroszewska <katarzyna_jaroszewska@wp.eu>
2020-01-26 10:55:43 +0100

    Commit of calculator operations update (division, power, percentage)

---
+++ src/main/java/pl/sda/spring/exception/DivisionByZeroException.java
@@ -0,0 +1,7 @@
+package pl.sda.spring.exception;
+
+public class DivisionByZeroException extends RuntimeException {
+    public DivisionByZeroException(String message) {
+        super(message);
+    }
+}
--- src/test/java/pl/sda/spring/CalculatorTest.java
+++ src/test/java/pl/sda/spring/CalculatorTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pl.sda.spring.exception.DivisionByZeroException;
 import pl.sda.spring.operation.OperationType;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -26,5 +27,79 @@ public class CalculatorTest {
 
         //then
         Assert.assertEquals(5.0, actual, 0.00000001);
+    }
+
+    @Test
+    public void shouldReturnTenForGivenArgumentsOnSubtractionOperation() {
+        //given
+        double arg1 = 15.0;
+        double arg2 = 5.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.SUBTRACTION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(10.0, actual, 0.0000001);
+    }
+
+    @Test
+    public void shouldReturnFortyForGivenArgumentsOnMultiplicationOperation() {
+        //given
+        double arg1 = 4.0;
+        double arg2 = 10.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.MULTIPLICATION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(40.0, actual,0.0000001);
+    }
+    @Test
+    public void shouldReturnTenForGivenArgumentsOnDivisionOperation() {
+        //given
+        double arg1 = 100.0;
+        double arg2 = 10.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.DIVISION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(10.0, actual,0.0000001);
+    }
+
+    @Test(expected = DivisionByZeroException.class)
+    public void shouldThrowExceptionWhenDivisionByZero() {
+        //given
+        double arg1 = 100.0;
+        double arg2 = 0.00;
+
+        //when
+        double actual = calculator.calculate(OperationType.DIVISION, arg1, arg2);
+    }
+
+    @Test
+    public void shouldReturnFiveForGivenArgumentsOnPercentageOperation() {
+        //given
+        double arg1 = 100.0;
+        double arg2 = 5.00;
+
+        //when
+        double actual = calculator.calculate(OperationType.PERCENTAGEOPERATION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(5.0, actual,0.0000001);
+    }
+
+    @Test
+    public void shouldReturnSixteenForGivenArgumentsOnPowerOperation() {
+        //given
+        double arg1 = 2.00;
+        double arg2 = 4.00;
+
+        //when
+        double actual = calculator.calculate(OperationType.POWEROPERATION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(16.0, actual,0.0000001);
     }
 }
